c439b7926d6a7b6d658cbaca4e531d293ad8ead0
akshat31 <akshat.soni31@gmail.com>
2018-08-15 18:16:54 +0530

    yoga-cooking classes website project

---
+++ yoga-cooking/xtra/contact.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>YOGA COOKING CLASSES ::Yoga , patnem beach goa, cooking classes,Rahul cooking, rooms in goa, patnem, kranti yoga, Lotus Yoga Retreat - South Goa , bamboo yoga, retreat in goa, vinyasa  ,ashtanga ,hatha   ,kundalini,  mysore style, Sampoorna Yoga, Shiva Yoga Centre - Agonda, Palolem goa, sea view , south goa, Dabolim international airport</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Yoga , patnem beach goa, cooking classes,Rahul cooking, rooms in goa, patnem, kranti yoga, Lotus Yoga Retreat - South Goa , bamboo yoga, retreat in goa, vinyasa  ,ashtanga ,hatha   ,kundalini,  mysore style, Sampoorna Yoga, Shiva Yoga Centre - Agonda, Palolem goa, sea view , south goa, Dabolim international airport" />
+<meta name="description" content="Indian cooking and yoga have originated from the science of living a healthy and orderly life Learn the basics of this science and learn how to use these concepts to create these eating habits and to balance health-flavors in everyday life,This class is for everyone including beginners, who doesn' cook anything, yoga practitioners, yoga teachers, Those who want to eat healthy food and  like to learn about balanced food. our classes is open for everyone">
+<!-- //for-mobile-apps -->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<!-- pop-up -->
+<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //pop-up -->
+<link rel="stylesheet" type="text/css" href="css/zoomslider.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<script type="text/javascript" src="js/modernizr-2.6.2.min.js"></script>
+<!--/web-fonts-->
+<link href='//fonts.googleapis.com/css?family=Tangerine:400,700' rel='stylesheet' type='text/css'>
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<!--//web-fonts-->
+</head>
+<body>
+<!--/main-header-->
+  <!--/banner-section-->
+	<div id="demo-1" class="banner-inner">
+		<!--/header-w3l-->
+			   <div class="header-w3-agileits" id="home">
+			     <div class="inner-header-agile">	
+								<nav class="navbar navbar-default">
+									<div class="navbar-header">
+										<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+											<span class="sr-only">Toggle navigation</span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+											<span class="icon-bar"></span>
+										</button>
+										<h1><a  href="index.html"><span>YOGA</span><p class="s-log">Cooking Classes</p></a>
+										 
+										</h1>
+									</div>
+									<!-- navbar-header -->
+									<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+										
+				 						<ul class="nav navbar-nav">
+											<li><a href="index.html">Home</a></li>
+												<li><a href="about.html">About</a></li>
+												<li><a href="booking.html">Booking Classes</a></li>
+												<li><a href="gallery.html">Gallery</a></li>
+												 <!-- <li class="dropdown">
+													<a href="codes.html" class="dropdown-toggle hvr-bounce-to-bottom" data-hover="Pages" data-toggle="dropdown" aria-expanded="false">Pages <b class="caret"></b></a>
+													 <ul class="dropdown-menu">
+														<li><a href="icons.html">Font Icons</a></li>
+													
+														<li><a href="codes.html">Short Codes</a></li>
+													</ul>
+												</li> -->
+
+												<li class="active"><a href="contact.html">Contact</a></li>
+
+											
+										</ul>
+
+
+									</div>
+									<div class="clearfix"> </div>	
+								</nav>
+									<!-- <div class="w3ls_search">
+													<div class="cd-main-header">
+														<ul class="cd-header-buttons">
+															<li><a class="cd-search-trigger" href="#cd-search"> <span></span></a></li>
+														</ul>
+													</div>
+													<div id="cd-search" class="cd-search">
+														<form action="#" method="post">
+															<input name="Search" type="search" placeholder="Search...">
+														</form>
+													</div>
+												</div> -->
+					
+							</div> 
+
+			
+		<!--//header-w3l-->
+			<!--/banner-info-->
+			<!--/banner-ingo-->
+			
+		</div>
+		 </div>
+  <!--/banner-section-->
+ <!--//main-header-->
+	        
+						<!-- breadcrumb -->
+	<!-- <div class="w3_breadcrumb">
+	<div class="breadcrumb-inner">	
+			<ul>
+				<li><a href="index.html">Home</a> <i>//</i></li>
+				
+				<li>Contact</li>
+			</ul>
+		</div>
+	</div> -->
+<!-- //breadcrumb -->
+			<!--/content-inner-section-->
+				  <div class="w3_content_agilleinfo_inner">
+					    <div class="container">
+							<div class="inner-agile-w3l-part-head">
+					           <h2 class="w3l-inner-h-title">Contact</h2>
+								
+							</div>
+									<div class="w3_mail_grids">
+										<form action="send_form_email.php" name="contactform" method="post">
+											<div class="col-md-6 w3_agile_mail_grid">
+													<input type="text" name="first_name" placeholder="First Name"  required="">
+													<br>
+													<br>
+													<input type="text" name="last_name" placeholder="Last Name"  required="">
+													<input type="email" name="email" placeholder="Your Email" required="">
+													<input type="text" name="telephone" placeholder="Your Phone Number" required="">
+
+												
+											</div>
+											<div class="col-md-6 w3_agile_mail_grid">
+												<textarea name="comments" placeholder="Your Message" required=""></textarea>
+												<input type="submit" value="Submit">
+											</div>
+											<div class="clearfix"> </div>
+										</form>
+							</div>
+					   </div>
+							<div class=" map">
+								<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3853.897083318972!2d74.03204031484499!3d14.99841718955007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTTCsDU5JzU0LjMiTiA3NMKwMDInMDMuMiJF!5e0!3m2!1sen!2s!4v1506911359986" frameborder="0" style="border:0" allowfullscreen></iframe>
+							</div>
+				  </div>
+			<!--//content-inner-section-->
+			<div class="w3l_contact-bottom">
+		<div class="container">
+			
+			<div class="w3ls_con_grids">
+				
+				<div class="w3ls_footer_grid">
+					<div class="col-md-4 con-ions-left">
+						<div class="con-ions-left-w3l">
+							<i class="fa fa-map-marker" aria-hidden="true"></i>
+						</div>
+						<div class="con-grid-w3l-leftr">
+							<h4>Location</h4>
+							<p>3030 New York, NY, USA</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="col-md-4 con-ions-left">
+						<div class="con-ions-left-w3l">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</div>
+						<div class="con-grid-w3l-leftr">
+							<h4>Email</h4>
+							<a href="mailto:info@example.com">info@example.com</a>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="col-md-4 con-ions-left">
+						<div class="con-ions-left-w3l">
+							<i class="fa fa-phone" aria-hidden="true"></i>
+						</div>
+						<div class="con-grid-w3l-leftr">
+							<h4>Call Us</h4>
+							<p>(+000) 003 003 0052</p>
+						</div>
+						<div class="clearfix"> </div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+		</div>
+	</div>
+ <!-- Footer -->
+	<div class="w3l-footer">
+		<div class="container">
+         <div class="footer-info-agile">
+         		<div class="col-md-3 footer-grid">
+         		<h4>About us</h4>
+         		<p>Basic Workshop are designed solely with the student in mind. Our  Workshop provide students with a thorough understanding of the material, so that they are able to tackle problems in the real world</p>
+				   
+				</div>
+
+
+				<div class="col-md-2 footer-info-grid links">
+					<h4>Quick links</h4>
+					<ul>
+						       <li><a href="index.html">Home</a></li> 
+								<li><a href="about.html">About</a></li> 
+								<li><a href="codes.html">Services</a></li> 
+								<li><a href="gallery.html">Gallery</a></li> 
+								<li><a href="contact.html">Contact</a></li> 
+					</ul>
+				</div>
+
+				
+				<div class="col-md-3 footer-info-grid address">
+					<h4>Address</h4>
+					<address>
+						<ul>
+							<li>Patnem Beach,</li>
+							<li> Goa</li>
+							<li>Telephone : 9098531434</li>
+							<li>Email : <a class="mail" href="mailto:yogacookingclasses@gmail.com">yogacookingclasses@gmail.com</a></li>
+						</ul>
+					</address>
+				</div>
+				
+				<div class="col-md-4 footer-info-grid">
+				<div class="connect-social">
+					<h4>Connect with us</h4>
+					<section class="social">
+                        <ul>
+							<li><a class="icon fb" href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a class="icon tw" href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a class="icon pin" href="#"><i class="fa fa-pinterest"></i></a></li>
+							<li><a class="icon db" href="#"><i class="fa fa-dribbble"></i></a></li>
+							<li><a class="icon gp" href="#"><i class="fa fa-google-plus"></i></a></li>
+						</ul>
+					</section>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+	   </div>
+     </div>
+
+		
+			<div class="w3agile_footer_copy">
+				    <p>© 2017 YOGA COOKING CLASSES. All rights reserved</p>
+			</div>
+		<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+	<!-- Dropdown-Menu-JavaScript -->
+			<script>
+				$(document).ready(function(){
+					$(".dropdown").hover(            
+						function() {
+							$('.dropdown-menu', this).stop( true, true ).slideDown("fast");
+							$(this).toggleClass('open');        
+						},
+						function() {
+							$('.dropdown-menu', this).stop( true, true ).slideUp("fast");
+							$(this).toggleClass('open');       
+						}
+					);
+				});
+			</script>
+		<!-- //Dropdown-Menu-JavaScript -->
+
+
+<script type="text/javascript" src="js/jquery.zoomslider.min.js"></script>
+		<!-- search-jQuery -->
+				<script src="js/main.js"></script>
+			<!-- pop-up-box -->  
+		<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+	<!--//pop-up-box -->
+
+			<div id="small-dialog1" class="mfp-hide">
+		<iframe src="https://player.vimeo.com/video/36382067"  frameborder="0" ></iframe>
+	</div>
+	<div id="small-dialog2" class="mfp-hide">
+		<iframe src="https://player.vimeo.com/video/36382067"  frameborder="0" ></iframe>
+	</div>
+	<script>
+		$(document).ready(function() {
+		$('.w3_play_icon,.w3_play_icon1,.w3_play_icon2').magnificPopup({
+			type: 'inline',
+			fixedContentPos: false,
+			fixedBgPos: true,
+			overflowY: 'auto',
+			closeBtnInside: true,
+			preloader: false,
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'my-mfp-zoom-in'
+		});
+																		
+		});
+	</script>
+
+<!--/script-->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+
+<script type="text/javascript">
+			jQuery(document).ready(function($) {
+				$(".scroll").click(function(event){		
+					event.preventDefault();
+					$('html,body').animate({scrollTop:$(this.hash).offset().top},900);
+				});
+			});
+</script>
+ <script type="text/javascript">
+						$(document).ready(function() {
+							/*
+							var defaults = {
+					  			containerID: 'toTop', // fading element id
+								containerHoverID: 'toTopHover', // fading element hover id
+								scrollSpeed: 1200,
+								easingType: 'linear' 
+					 		};
+							*/
+							
+							$().UItoTop({ easingType: 'easeOutQuart' });
+							
+						});
+					</script>
+<!--end-smooth-scrolling-->
+<!--js for bootstrap working-->
+	<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+
+
+ 
+
+</body>
+</html>
